938772f46b5344bb3b8eea3e588480886243635f
MickaelP <mp041@hotmail.fr>
2026-06-11 22:08:01 -0400

    Integrate OnboardingTutorial component into App for first-time user guidance

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,25 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 import Title from './components/Title';
 import Navigation from './components/Navigation';
 import LanguageSelector from './components/LanguageSelector';
 import Amortizer from './components/Amortizer';
 import Compounder from './components/Compounder';
+import OnboardingTutorial from './components/OnboardingTutorial';
 import './App.scss';
 
 function App() {
     const { t } = useTranslation();
     const [activeMenu, setActiveMenu] = useState<string>('amortizer');
+    const [showTutorial, setShowTutorial] = useState<boolean>(false);
+
+    useEffect(() => {
+        const isFirstVisit = localStorage.getItem('isFirstVisit');
+        if (!isFirstVisit) {
+            setShowTutorial(true);
+            localStorage.setItem('isFirstVisit', 'false');
+        }
+    }, []);
 
     const renderActiveComponent = () => {
         switch (activeMenu) {
@@ -24,6 +34,7 @@ function App() {
 
     return (
         <div className="app">
+            {showTutorial && <OnboardingTutorial onComplete={() => setShowTutorial(false)} />}
             <div className="app__container">
                 <div className="app__header">
                     <Title />
@@ -32,10 +43,7 @@ function App() {
                 <div className="app__content">
                     <p className="app__subtitle">{t('app.subtitle')}</p>
                 </div>
-                <Navigation
-                    activeMenu={activeMenu}
-                    onMenuChange={setActiveMenu}
-                />
+                <Navigation activeMenu={activeMenu} onMenuChange={setActiveMenu} />
                 <div className="app__calculator">{renderActiveComponent()}</div>
             </div>
         </div>
